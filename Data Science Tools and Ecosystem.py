6cd5a3427fc18d92b7ee138ae5d080fb5ebf5f04
HatterDMad <131985313+HatterDMad@users.noreply.github.com>
2025-01-08 11:35:18 -0600

    Add files via upload

---
+++ Data Science Tools and Ecosystem.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Data Science Tools and Ecosystem
+# 
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[4]:
+
+
+(3*4)+5
+
+
+# In[7]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+200 / 60
+
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science
+# - Demonstrate basic arithmetic expression evaluation in Python
+# - Explain how to create headings in Markdown
+# - Show how to format lists in Markdown
+# - Introduce the concept of Data Science tools and ecosystems
+
+# ## Author
+# John Doe
+
+# In[ ]:
+
+
+
+
